cb014b940d0445f7a6c4f86a6536f342f7e35143
Vedhak6 <Vedhak6>
2021-02-15 21:46:36 +0530

    database connection

--- java/com/xworkz/mysql/customerTester.java
+++ java/com/xworkz/mysql/customerTester.java
@@ -1,38 +1,46 @@
 package com.xworkz.mysql;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
+import java.sql.SQLException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class customerTester {
 
-	public static void main(String[] args) {
-		ApplicationContext container=new ClassPathXmlApplicationContext("util.xml");
-		DriverManagerDataSource driverConnection=container.getBean(DriverManagerDataSource.class);
-		try {
-			Connection con=driverConnection.getConnection();
-			Statement stmt=con.createStatement();
-			String qry="insert into customer values('vedha',11,'Bangalore',987896778)";
-			String qry1="select * from customer";
+	public static void main(String[] args) throws SQLException {
 		
-		ResultSet rs=	stmt.executeQuery(qry);
-		while(rs.next()){
-			System.out.println(rs.toString());
-			
-		}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		String contextConfigLocation = "database.xml";
+		ApplicationContext container = new ClassPathXmlApplicationContext(contextConfigLocation);
+		System.out.println(container);
+		DriverManagerDataSource dm = container.getBean(DriverManagerDataSource.class);
 
+		Connection connection = dm.getConnection();
+		System.out.println(connection);
+
+	
 		
 
 	}
 
 
 	}
+
+//ApplicationContext container=new ClassPathXmlApplicationContext("database.xml");
+//DriverManagerDataSource driverConnection=container.getBean(DriverManagerDataSource.class);
+//try {
+//	Connection con=driverConnection.getConnection();
+//	Statement stmt=con.createStatement();
+//	String qry="insert into customer values('vedha',11,'Bangalore',987896778)";
+//	
+//
+//ResultSet rs=	stmt.executeQuery(qry);
+//while(rs.next()){
+//	System.out.println(rs.toString());
+//	
+//}
+//	
+//} catch (SQLException e) {
+//	e.printStackTrace();
+//}
